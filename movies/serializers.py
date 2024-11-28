cc5f68cee435a4996e6c3e5ed7d5d5e68079355b
Keven9801 <skeven98@gmail.com>
2024-11-28 10:26:48 -0300

    primeiro commit da api

---
+++ movies/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from movies.models import Movie
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+    def get_rate(self, obj):
+        ...
+        
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1900 !')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError('resumo não pode ter mais do que 500 caracteres !')
+        return value
+    
